6247a99580274b3948511ef643eada014c91d9a4
Bartosz Wieslaw Woronicz <bartosz.woronicz@nokia.com>
2017-06-23 15:08:37 +0200

    initial .templates dir

---
+++ .templates/python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='{% name %}',
+      version='{% version %}',
+      description='{% description %}',
+      url='{% url %}',
+      author='{% author %}',
+      author_email='{% author_email %}',
+      license='{% license %}',
+      packages=['{% packages %}'],
+      zip_safe=False)
