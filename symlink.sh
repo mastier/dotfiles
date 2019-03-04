c21f7435a87fbf628cd10560a4d46addf94cc663
Bartosz "mastier" Woronicz <(none@none)>
2019-03-04 16:52:24 +0100

    add mc and .config support

--- symlink.sh
+++ symlink.sh
@@ -6,30 +6,49 @@ shopt -s dotglob
 # make glob return emptystring on nomatch
 shopt -s nullglob
 
+main() {
 # for any dotfile except .. and .
+echo "Installing dotfiles..."
 for dotfile in .[^.]?*; do
-    case "$dotfile" in
-        .git) continue ;;
-        .gitmodules) continue ;;
-        *.sw*) continue ;;
-    esac
-    
-    target="$HOME/$dotfile"
-    dotfile="$PWD/$dotfile"
+  case "$dotfile" in
+    .git) continue ;;
+    .gitmodules) continue ;;
+    *.sw*) continue ;;
+  esac
 
-    if [[ -L "$target" ]] && [[ "$(readlink "$target")" == "$dotfile" ]]; then
-        echo "skipping: $dotfile"
+  target="$HOME/$dotfile"
+  dotfile="$PWD/$dotfile"
+  install_symlink "$dotfile" "$target"
+done
+
+# for listed config
+echo "Installing configs..."
+for conf in config/*; do
+  target="$HOME/.config/$(basename "$conf")"
+  conf="$PWD/$conf"
+  install_symlink "$conf" "$target"
+done
+}
+
+install_symlink() {
+  local src="$1"
+  local target="$2"
+
+  if [[ -L "$target" ]] && [[ "$(readlink "$target")" == "$src" ]]; then
+    echo "skipping: $src"
 	continue
     elif [[ -f "$target" ]] || [[ -d "$target" ]]; then
-        echo "move: $dotfile -> ${dotfile}.dist"
-        mv "$target" "$target.dist"
+      echo "move: $src -> ${source}.dist"
+      mv "$target" "$target.dist"
     fi
 
     if ! [[ -e "$target" ]]; then
-        echo "install: $dotfile -> $target"
-        ln -sf "$dotfile" "$target"
+      echo "install: $src -> $target"
+      ln -sf "$src" "$target"
     else
-        echo "PROBLEMO: Already exist and ain't symlink, regular file or dir: $dotfile "
-        exit 1
+      echo "PROBLEMO: Already exist and ain't symlink, regular file or dir: $src "
+      exit 1
     fi
-done
+}
+
+main
